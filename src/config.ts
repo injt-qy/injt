679bc05f7db9638e28292a5ca5e9868c45005084
injt-qy <niejiatong88@gmail.com>
2026-02-28 04:03:36 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -448,8 +448,8 @@ export const shareConfig: ShareConfig = {
 };
 
 export const announcementConfig: AnnouncementConfig = {
-	title: "", // 公告标题，填空使用i18n字符串Key.announcement
-	content: "ブログへようこそ！これはサンプルの告知です", // 公告内容
+	title: "公告", // 公告标题，填空使用i18n字符串Key.announcement
+	content: "安静更新 谢绝打扰", // 公告内容
 	closable: true, // 允许用户关闭公告
 	link: {
 		enable: true, // 启用链接
