df0b0fc6c75a2f7aa936058ff9f899c28be7b50e
injt-qy <niejiatong88@gmail.com>
2026-02-28 05:02:19 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -450,10 +450,10 @@ export const shareConfig: ShareConfig = {
 export const announcementConfig: AnnouncementConfig = {
 	title: "公告", // 公告标题，填空使用i18n字符串Key.announcement
 	content: "安静更新 谢绝打扰", // 公告内容
-	closable: true, // 允许用户关闭公告
+	closable: false, // 允许用户关闭公告
 	link: {
 		enable: true, // 启用链接
-		text: "Learn More", // 链接文本
+		text: "免责声明", // 链接文本
 		url: "/about/", // 链接 URL
 		external: false, // 内部链接
 	},
