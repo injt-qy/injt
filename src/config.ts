45a6016e4ba9ed4f0f79a4e8fa93bc4a782290e2
injt-qy <niejiatong88@gmail.com>
2026-02-27 23:44:31 +0800

    更新 config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-import type {
+1import type {
 	AnnouncementConfig,
 	CommentConfig,
 	ExpressiveCodeConfig,
@@ -25,7 +25,7 @@ export const siteConfig: SiteConfig = {
 	title: "Mizuki",
 	subtitle: "One demo website",
 	siteURL: "https://mizuki.mysqil.com/", // 请替换为你的站点URL，以斜杠结尾
-	siteStartDate: "2025-01-01", // 站点开始运行日期，用于站点统计组件计算运行天数
+	siteStartDate: "2022-03-03", // 站点开始运行日期，用于站点统计组件计算运行天数
 
 	timeZone: SITE_TIMEZONE,
 
